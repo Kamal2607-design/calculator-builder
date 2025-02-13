6bc900d13f044e05281beae103b6ead7da0b4c05
Kamalesh <kg20030726@gmail.com>
2025-02-13 12:06:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <h3>Hello</h3>
+      <h4>Hello</h4>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  <img className="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo" />
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+    <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
